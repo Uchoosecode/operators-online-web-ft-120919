ee87e4e373b5957b72add0fb3181f56eb6258369
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-09 23:18:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -13,11 +13,10 @@ end
 
 def not_safe?(speed)
   
-  if speed > 60 
-     true 
-  elsif speed < 40
-     true
-  else    
+  if speed > 60 && speed < 40 ?
+     true :
+     false
+   end
 end
 	
 
